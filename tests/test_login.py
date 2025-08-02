ff5736c64b0d8b3280a77713ced2c8fa6f35e9b8
amruta.ks <amruta.ks2022cse@sece.ac.in>
2025-08-02 15:49:04 +0530

    Page object model

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,7 +7,7 @@ driver.get("https://skillrack.com/faces/ui/profile.xhtml")
 login_page=Login(driver)
 
 if(login_page.is_loginpage_displayed()):
-    login_page.login_meth("hello","123")
+    login_page.login_meth("22cs018","123")
     print(login_page.get_error_msg())
 else:
     print("Login page not displayed")
